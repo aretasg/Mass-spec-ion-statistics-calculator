068f0c92e27f9123ddde5c66fa13abfe9664ec1e
Aretas Gaspariunas <aretasgasp@gmail.com>
2017-05-02 21:56:11 +0100

    Update and rename one_script.py to MScalculator.py

--- MScalculator.py
+++ MScalculator.py
@@ -1,34 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-#sliding window PLEASE READ!!!!
-# arguments: -f for filename; -from -to for range; -ppm if to provide accuracy in ppm; -Da for acuracy in daltons; -w for window_size; -m for mode of search; -p for pattern of interest
+#PLEASE READ!!!!
+# arguments: -f for filename; -from -to for range; -ppm to provide accuracy in ppm; -Da for acuracy in daltons; -w for window_size; -m for mode of search; -p for pattern of interest
 # -m and -p are not required if there is no pattern of interest.
 
-import argparse, re
+import argparse
+import re
 
 value_list = []
 bins = {}
-aa_score_list = []
-error_list = ['B', 'J', 'O', 'U', 'X', 'Z']
+aa_score_list = []  #sequence that have pattern of interest 
+error_list = ['B', 'J', 'O', 'U', 'X', 'Z']  #letters not used to mark amino acids
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-f', '--file',
-    help='chose the file', required=True)
+    help='chose the file', required = True)
 parser.add_argument('-from', '--from_range',
-    help='chose the range. From:', default=1000)
+    help='chose the range. From:', default = 1000)
 parser.add_argument('-to', '--to_range',
-    help='chose the range. To:', default=1500)
+    help='chose the range. To:', default = 1500)
 parser.add_argument('-ppm', '--parts_per_million',
     help='enter the value for ppm')
 parser.add_argument('-Da', '--Da_accuracy',
-    help='enter the value for mass accuracy in Da', default=0.2)
+    help='enter the value for mass accuracy in Da', default = 0.2)
 parser.add_argument('-w', '--window_size',
-    help='define the window size', required=True)
+    help='define the window size', required = True)
 parser.add_argument('-m', '--mode',
     help='chose the mode of search [any, start, end]')
 parser.add_argument('-p', '--pattern',
-    help='define the pattern ')
+    help='define the pattern')
 
 
 args = parser.parse_args()
@@ -37,7 +38,7 @@ x = float(args.from_range)
 y = float(args.to_range)
 Da = args.Da_accuracy
 
-def search_any(input_data, pattern):
+def search_any(input_data, pattern): #a function to search data for patterns. Starting with/ending with or just containing them anywhere
     print ('Peptide ions containing {0}:'.format(pattern))
     for line in input_data:
         line = line.rstrip("\n")
@@ -48,7 +49,7 @@ def search_any(input_data, pattern):
             aa_score_list.append(line)
 
             
-def search_start(input_data, pattern):
+def search_start(input_data, pattern): #a function to search data for sequences starting with the residues provided with -p
     print ('Peptide ions starting with {0}:'.format(pattern))
     for line in input_data:
         line = line.rstrip("\n")
@@ -59,7 +60,7 @@ def search_start(input_data, pattern):
             aa_score_list.append(line)
 
 
-def search_end(input_data, pattern):
+def search_end(input_data, pattern): #a funtion to search data for sequences ending with the residues provided with -p
     print ('Peptide ions ending with {0}:'.format(pattern))
     for line in input_data:
         line = line.rstrip("\n")
@@ -69,21 +70,21 @@ def search_end(input_data, pattern):
         if m_aa:
             aa_score_list.append(line)
 
-try:
-    data = open(args.file).readlines()    #Error catch
+try:                                        #attempts to open and read the input
+    data = open(args.file).readlines()
 except IOError:
-    print ('Could not open file',args.file)
+    print ('Could not open file',args.file)  #Displays an error message if unsuccessfull
     exit()
 
-if float(args.from_range) == 0 and args.parts_per_million:  #Error catch
-    print ('You cannot use range from 0 with ppm! Try using a number close to null instead')
+if float(args.from_range) == 0 and args.parts_per_million:  #Checks if range starts with 0 when ppm accuracy is selected
+    print ('You cannot use range starting with 0 with ppm option! Try using a number close to null instead')
     exit()
 
-if args.mode and args.pattern:
+if args.mode and args.pattern:        #Checks if the pattern is sensible and can be used to search the data
     aa = args.pattern.upper()
     for element in error_list:
-        match_res = re.search(r'{0}'.format(element), aa)
-        match_res1 = re.search(r'[0-9]+',aa)
+        match_res = re.search(r'{0}'.format(element), aa)  #checks if the pattern has any letters not representing amino acids
+        match_res1 = re.search(r'[0-9]+',aa)  #checks if the pattern has any numbers (amino acids cannot be represented as numbers
     if match_res:
          print('Please enter the correct residues. Amino acids are not represented by {0}'.format(error_list))
          exit()
@@ -91,6 +92,8 @@ if args.mode and args.pattern:
          print('Residues cannot be represented by numbers!')
          exit()
 
+#Depending on the preference speciefied in CLI, one out of three pattern search options are selected and
+#a respective function is called
 if args.mode == 'any':
     search_any(data, aa)
 elif args.mode == 'start':
@@ -98,31 +101,31 @@ elif args.mode == 'start':
 elif args.mode == 'end':
     search_end(data, aa)
             
-
-def range_(data, rfrom, rto, list_):
-    for i in data:
+def range_(data, rfrom, rto, list_):  #a function to iterate over the input; compare m/z values to a specified range (-form/-to)
+    for i in data:                    #add m/z value that belong in the range
         f = i.split()
         z = float(f[2])
         if z >= rfrom and z <= rto:
             list_.append(z)
 
-if args.mode and args.pattern and aa_score_list == []:
+if args.mode and args.pattern and aa_score_list == []:  #prints a message if no sequences match the pattern of interest
     print ('NO MATCHES WITH THE PATTERN')
     exit()
-elif args.mode and args.pattern:
+elif args.mode and args.pattern:  #
     range_(aa_score_list, x, y, value_list)
 else:
     range_(data, x, y, value_list)
 
-if value_list == []:
-    print ('No values in the given mass range!')  #Error catch
+if value_list == []:        ##Prints a warning message if there are no values in the m/z range specified 
+    print ('No values in the given mass range!')
     exit()
 #print (value_list, len(value_list)) #checkpoint
         
-s = sorted(value_list)
+s = sorted(value_list)  #sorts the value_list
 
 m2 = x + window_size
 m1 = m2 - window_size
+
 while m2 <= y:
     for i in s:
         if m2 not in bins.keys():
